d6827ebb50c30d4bb92d3c3771d82dea42a2a4bf
Simon Palmberg <simon.palmberg@gmail.com>
2022-12-17 15:14:45 +0100

    Added title to error pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Image from "next/image";
 import CenteredContent from "../components/CenteredContent";
 
 export default function Custom404() {
   return (
     <CenteredContent>
+      <Head>
+        <title>Sidan kan inte hittas - Nordic Trax</title>
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
       <Image src="/images/logo.jpg" alt="Nordic Trax" width="311" height="65" />
       <h1>404 - Sidan kan inte hittas</h1>
     </CenteredContent>
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Image from "next/image";
 import CenteredContent from "../components/CenteredContent";
 
 export default function Custom500() {
   return (
     <CenteredContent>
+      <Head>
+        <title>Internt servefel - Nordic Trax</title>
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
       <Image src="/images/logo.jpg" alt="Nordic Trax" width="311" height="65" />
       <h1>500 - Serverfel</h1>
     </CenteredContent>
